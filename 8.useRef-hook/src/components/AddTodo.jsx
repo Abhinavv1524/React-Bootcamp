f7f119f6f95ebeedb5cdde21cc6e4cd2bad0d534
Abhinavv1524 <choudharyabhinav818@gmail.com>
2024-09-21 22:07:10 +0530

    Learned about a new hook that is useRef

---
+++ 8.useRef-hook/src/components/AddTodo.jsx
@@ -0,0 +1,41 @@
+import { useRef } from "react";
+import { IoMdAddCircleOutline } from "react-icons/io";
+export function AddTodo({ itemToBeAdded }) {
+  // let itemName = document.getElementById("nameValue");
+  // let itemDate = document.getElementById();
+
+  const itemNameElement = useRef(0);
+  const itemDateElement = useRef();
+
+  const handleButtonClicked = (event) => {
+    event.preventDefault();
+    //console.log(event);
+    const itemName = itemNameElement.current.value;
+    const itemDate = itemDateElement.current.value;
+    itemToBeAdded(itemName, itemDate);
+    itemDateElement.current.value;
+    itemNameElement.current.value;
+    console.log(itemName);
+  };
+  return (
+    <div className="container text-center">
+      <form className="row new-row" onSubmit={handleButtonClicked}>
+        <div className="col-4">
+          <input
+            type="text"
+            placeholder="Enter todo here"
+            ref={itemNameElement}
+          />
+        </div>
+        <div className="col-4">
+          <input type="date" name="" id="" ref={itemDateElement} />
+        </div>
+        <div className="col-2">
+          <button className="btn btn-success new-button">
+            <IoMdAddCircleOutline />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ 8.useRef-hook/src/components/AppName.jsx
@@ -0,0 +1,4 @@
+import styles from "./AppName.module.css";
+export function AppName() {
+  return <h1 className={styles.todoheading}>Todo App</h1>;
+}
