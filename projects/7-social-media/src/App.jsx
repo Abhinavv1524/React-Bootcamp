da10b19f462e4abe305aea2439ae488705b9cd48
Abhinavv1524 <choudharyabhinav818@gmail.com>
2024-09-26 23:40:27 +0530

    Updaiting the files

--- projects/7-social-media/src/App.jsx
+++ projects/7-social-media/src/App.jsx
@@ -6,24 +6,27 @@ import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/CreatePost";
 import PostList from "./components/postList";
 import { useState } from "react";
+import PostListProvider from "./store/Post-list-store";
 function App() {
   const [selectedTab, setSelectedTab] = useState("Home");
   return (
-    <div className="app-container">
-      <Sidebar
-        selectedTab={selectedTab}
-        setSelectedTab={setSelectedTab}
-      ></Sidebar>
-      <div className="content">
-        <Header></Header>
-        {selectedTab === "Home" ? (
-          <PostList></PostList>
-        ) : (
-          <CreatePost></CreatePost>
-        )}
-        <Footer></Footer>
+    <PostListProvider>
+      <div className="app-container">
+        <Sidebar
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        ></Sidebar>
+        <div className="content">
+          <Header></Header>
+          {selectedTab === "Home" ? (
+            <PostList></PostList>
+          ) : (
+            <CreatePost></CreatePost>
+          )}
+          <Footer></Footer>
+        </div>
       </div>
-    </div>
+    </PostListProvider>
   );
 }
 
---
+++ projects/7-social-media/src/store/Post-list-store.jsx
@@ -0,0 +1,53 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const PostListReducer = (currPostList, action) => {
+  return currPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    PostListReducer,
+    DEFAULT_POST_VALUE
+  );
+
+  const addPost = () => {};
+
+  const deletePost = () => {};
+  return (
+    <PostList.Provider
+      value={{
+        postList,
+        addPost,
+        deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+const DEFAULT_POST_VALUE = [
+  {
+    id: "1",
+    title: "Going to college",
+    body: "college jaake bura haal hogya hai",
+    reactions: "2",
+    userId: "user-1",
+    tags: ["finally", "over"],
+  },
+  {
+    id: "2",
+    title: "Going canada",
+    body: "canada jaake or bhi zada bura haal hogya hai",
+    reactions: "10",
+    userId: "user-2",
+    tags: ["woof", "done"],
+  },
+];
+
+export default PostListProvider;
