2b04e3b8b6a97b8b9140868a253716241e0c38a0
Abhinavv1524 <choudharyabhinav818@gmail.com>
2024-10-01 22:41:16 +0530

    Learned about React Router

---
+++ 15.react-router/src/store/Post-list-store.jsx
@@ -0,0 +1,83 @@
+import { createContext, useEffect, useReducer, useState } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  fetching: false,
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const PostListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  } else if (action.type === "ADD_ALL_POSTS") {
+    newPostList = action.payload.posts;
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currPostList];
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(PostListReducer, []);
+  const [fetching, setFetching] = useState(false);
+
+  const addPost = (posts) => {
+    console.log(posts);
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: posts,
+    });
+  };
+
+  const addAllPosts = (posts) => {
+    dispatchPostList({
+      type: "ADD_ALL_POSTS",
+      payload: {
+        posts,
+      },
+    });
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  useEffect(() => {
+    setFetching(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetch("https://dummyjson.com/posts", { signal })
+      .then((res) => res.json())
+      .then((data) => {
+        addAllPosts(data.posts);
+        setFetching(false);
+      });
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return (
+    <PostList.Provider
+      value={{
+        postList,
+        fetching,
+        addPost,
+        deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+export default PostListProvider;
