84ba8968bd4ed8a49826a63af1bcc750d8ccb9b6
Abhinavv1524 <choudharyabhinav818@gmail.com>
2024-10-06 17:32:34 +0530

    Minor Changes

--- 21.counter-react-redux/src/components/Controls.jsx
+++ 21.counter-react-redux/src/components/Controls.jsx
@@ -1,7 +1,9 @@
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 const Controls = () => {
   const disptach = useDispatch();
+  const inputElement = useRef();
 
   const handleIncrement = () => {
     disptach({ type: "INCREMENT" });
@@ -10,23 +12,66 @@ const Controls = () => {
   const handleDecrement = () => {
     disptach({ type: "DECREMENT" });
   };
+
+  const handleAdd = () => {
+    disptach({
+      type: "ADD",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
+  const handleSubtract = () => {
+    disptach({
+      type: "SUBTRACT",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
   return (
-    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button
-        type="button"
-        className="btn btn-primary btn-lg px-4 gap-3"
-        onClick={handleIncrement}
-      >
-        +1
-      </button>
-      <button
-        type="button"
-        className="btn btn-success btn-lg px-4"
-        onClick={handleDecrement}
-      >
-        -1
-      </button>
-    </div>
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button
+          type="button"
+          className="btn btn-primary btn-lg px-4 gap-3"
+          onClick={handleIncrement}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          className="btn btn-success btn-lg px-4"
+          onClick={handleDecrement}
+        >
+          -1
+        </button>
+      </div>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center my-4">
+        <input
+          className="number-input"
+          type="text"
+          placeholder="Enter number"
+          ref={inputElement}
+        />
+        <button
+          type="button"
+          className="btn btn-info btn-lg px-4"
+          onClick={handleAdd}
+        >
+          Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger btn-lg px-4"
+          onClick={handleSubtract}
+        >
+          Subtract
+        </button>
+      </div>
+    </>
   );
 };
 
--- 21.counter-react-redux/src/store/index.js
+++ 21.counter-react-redux/src/store/index.js
@@ -9,7 +9,12 @@ const counterReducer = (store = INITIAL_vALUE, action)=>{
         return {counter: store.counter + 1}
     }else if(action.type === "DECREMENT"){
         return {counter: store.counter - 1}
+    }else if(action.type === "ADD"){
+        return {counter: store.counter + Number(action.payload.num)}
+    }else if(action.type === "SUBTRACT"){
+        return {counter: store.counter - Number(action.payload.num)}
     }
+
     return store;
 }
 
