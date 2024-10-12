9a200d1c22bb6b7efc13dd2b7f19a81561d64b31
Abhinavv1524 <choudharyabhinav818@gmail.com>
2024-10-12 17:35:26 +0530

    Updating the project

--- projects/9-myntra-clone/myntra-clone/src/routes/App.jsx
+++ projects/9-myntra-clone/myntra-clone/src/routes/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import FetchItems from "../components/FetchItem";
 
 function App() {
   return (
     <div>
       <Header />
+      <FetchItems />
       <Outlet />
       <Footer />
     </div>
